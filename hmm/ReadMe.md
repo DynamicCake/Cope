ce8f3c036f42697b32e0c8d3e0dd984b7d637438
DynamicCake <mcdynamiccake@gmail.com>
2023-07-02 16:48:03 +0800

    Combo support

---
+++ hmm/ReadMe.md
@@ -0,0 +1,13 @@
+# Hmm
+
+## What is this?
+This folder holds all my notes for brainstorming, planning, and just thinking.
+This helps me to remember what I was thinking when I come back to continue this, or to cringe on this.
+
+If this project, on the off chance, becomes popular, or even gets *one* contributor. I will create an actual wiki and discussion thing. 
+
+hmm v1.0
+
+---
+
+This is my first time trying this so...
--- src/parse.rs
+++ src/parse.rs
@@ -1,17 +1,16 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-
-use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-    pub version: String, 
+    pub version: String,
 
-    pub default_list: String,
+    pub default_combo: String,
     pub default_start: String,
-    
+
     pub separator: String,
-    
+
     pub message_list: HashMap<String, Vec<String>>,
     pub start_list: HashMap<String, Vec<String>>,
+    pub combos: HashMap<String, Vec<String>>,
 }
-
